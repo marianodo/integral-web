740a959b90031a0347c2cf17ddf0cab54a6f5775
Mariano Dominguez <mariano@destinilocators.com>
2023-01-17 17:38:15 -0300

    adding first files

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import jsonify
+
+from config import config
+
+def create_app(enviroment):
+    app = Flask(__name__)
+
+    app.config.from_object(enviroment)
+
+    return app
+
+enviroment = config['development']
+app = create_app(enviroment)
+
+@app.route('/api/v1/get_messages', methods=['GET'])
+def get_users():
+    response = {'message': 'success'}
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
