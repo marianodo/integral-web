b3b034d0fbe2af46157c8103261ed4ecadc09ed3
Mariano Dominguez <mariano@destinilocators.com>
2023-01-17 21:00:21 -0300

    hardcode list

--- main.py
+++ main.py
@@ -15,7 +15,16 @@ app = create_app(enviroment)
 
 @app.route('/api/v1/get_messages', methods=['GET'])
 def get_users():
-    response = {'message': 'success'}
+    response = [
+        {"code": 4220, "date": "2023/01/10", "time": "20:20", "message": "Apertura de usuario"},
+        {"code": 4220, "date": "2023/01/10", "time": "18:20", "message": "Cierre de usuario"},
+        {"code": 4220, "date": "2023/01/09", "time": "20:20", "message": "Alarma habitación del dragon"},
+        {"code": 1220, "date": "2023/01/10", "time": "14:20", "message": "Acuario en orden"},
+        {"code": 4220, "date": "2023/01/08", "time": "15:10", "message": "Bau bauu"},
+        {"code": 1220, "date": "2023/01/08", "time": "08:20", "message": "El acuario cerrado"},
+        {"code": 1220, "date": "2023/01/07", "time": "09:20", "message": "Como está todo en el Acuario?"},
+        {"code": 1220, "date": "2023/01/06", "time": "06:20", "message": "Acuario acá"}
+        ]
     return jsonify(response)
 
 @app.route('/', methods=['GET'])
