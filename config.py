4a423463493430d1ec5b0b6bc4d0f61115cdedf6
Mariano Dominguez <mardom4164@gmail.com>
2023-01-20 12:34:15 -0300

    Revert "Feature/add sqlalquemy connection"

--- config.py
+++ config.py
@@ -1,21 +1,9 @@
-from os import environ, path
-from dotenv import load_dotenv
-
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
-
-SECRET_KEY = environ.get('SECRET_KEY')
-USER_DB = environ.get('USER_DB')
-HOST = environ.get('HOST')
-PORT = environ.get('PORT')
-DATABASE = environ.get('DATABASE')
 class Config:
     pass
+
 class DevelopmentConfig(Config):
     DEBUG = True
-    
-    SQLALCHEMY_DATABASE_URI = \
-        f'mysql+mysqlconnector://{USER_DB}:{SECRET_KEY}@{HOST}:{PORT}/{DATABASE}?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/users'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
