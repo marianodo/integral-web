8406e343b86914e6d9448e00251829acc85e60fa
Mariano Dominguez <mariano@destinilocators.com>
2023-01-29 21:01:01 -0300

    add/update user. validate token

--- views.py
+++ views.py
@@ -1,13 +1,37 @@
-from flask import Blueprint
-from flask import jsonify
+from flask import Blueprint, jsonify, request, url_for
 from flask_cors import cross_origin
+from flask_mail import Message
+from itsdangerous import SignatureExpired, BadTimeSignature
+from werkzeug.security import generate_password_hash
 
-from extensions import db
+
+from extensions import db, mail, s
 from models import WebUsers, Clients, Messages
 from flask import current_app
 
-
 main = Blueprint('main', __name__)
+
+def send_token_to_email(email):
+    current_app.logger.info('Creating Token')
+    token = s.dumps(email, salt=email)
+    msg = Message("Email de Confirmación", sender=current_app.config["MAIL_USERNAME"], recipients=[email])
+    link = url_for("main.confirm_email", token=token, email=email, _external=True)
+    msg.body = f"El link es: {link}"
+    current_app.logger.info('Sending Email')
+    mail.send(msg)
+
+def add_update_web_user(email, password):
+    current_app.logger.info("Adding/updating user")
+    hash_password = generate_password_hash(password, method='sha256')
+
+    user = WebUsers.query.filter_by(email=email).first()
+    if not user: 
+        new_user = WebUsers(email=email, password=hash_password)
+        db.session.add(new_user)
+    else:
+        user.password = hash_password
+        user.is_confirmed = False
+    db.session.commit()
 
 
 @main.route('/api/v1/get_messages', methods=['GET'])
@@ -46,21 +70,27 @@ def get_last_message(client_id):
     return jsonify(last_message)
 
 
-@main.route('/new_user', methods=['POST'])
+@main.route('/signup', methods=['POST'])
 def register_new_user():
-    email = request.form["email"] 
-    token = s.dumps(email, salt="email-confirmation")
-    msg = Message("Email de Confirmación", sender=current_app.config["MAIL_USERNAME"], recipients=[email])
-    link = url_for("main.confirm_email", token=token, email=email, _external=True)
-    msg.body = f"El link es: {link}"
-    mail.send(msg)
-    return token
+    # Get params
+    email = request.form["email"]
+    password = request.form["password"]
+
+    add_update_web_user(email, password)
+    send_token_to_email(email)
+
+    return f"Token sent to {email}"
 
 @main.route("/confirm_email/<token>/<email>")
-def confirm_email(token,email):
+def confirm_email(token, email):
     try:
-        token_correct = s.loads(token, salt = "email-confirmation", max_age=60)
-        print(email)
+        s.loads(token, salt = email, max_age=60)
     except SignatureExpired:
         return "The token is expired"
+    except BadTimeSignature:
+        return "Bad Token"
+    
+    user = WebUsers.query.filter_by(email=email).first()
+    user.is_confirmed = True
+    db.session.commit()
     return "The token works"
