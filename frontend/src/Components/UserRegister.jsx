24f555b602a8a3b4c56ba05e6916aea1ea5d6cdd
diegomdomi <diegomdomi@gmail.com>
2023-02-04 16:27:50 +0100

    create registration form

--- frontend/src/Components/UserRegister.jsx
+++ frontend/src/Components/UserRegister.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-const UserRegister = () => {
+function UserRegister() {
   return (
-    <div>UserRegister</div>
-  )
+    <Form action='https://integral-web-production.up.railway.app/signup' method='POST'>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email </Form.Label>
+        <Form.Control type="email" placeholder="email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group> */}
+      <Button variant="primary" type="submit">
+        Enviar
+      </Button>
+    </Form>
+  );
 }
 
-export default UserRegister
+export default UserRegister;
